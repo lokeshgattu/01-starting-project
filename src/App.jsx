b67a7a124a976eac3292d602df2422b5f364feeb
lokeshgattu <lokeshgattappagari@lyrostech.com>
2024-06-20 10:13:09 +0530

    starting-react-app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Starting Project - 01</h1>
+        <h2> This is Lokesh's Project</h2>
+      </header> 
+    </div>
+  )
+}
+
+export default App
